047a7b3f691ac6ee603642a2fddadd0c88286828
Jeremy Linskill <jeremylinskill@gmail.com>
2026-09-03 23:05:32 -0400

    Temporarily disable notification response startup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -204,7 +204,6 @@ export default function RootLayout() {
                                     : DefaultTheme
                                 }>
                                 <StartupSplashController />
-                                <NotificationResponseController />
 
                                 <Stack
                                   screenOptions={{
